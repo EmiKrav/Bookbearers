9988768298fa9b0cb6bd777a700c868c2916e159
EmiKrav <126260437+EmiKrav@users.noreply.github.com>
2024-07-03 23:01:24 +0300

    Enemy movement

--- Code/GridMapMovement.gd
+++ Code/GridMapMovement.gd
@@ -2,118 +2,127 @@ extends GridMap
 
 @onready var langelis = preload("res://Bookbearers/Scenes/ejimolangelis.tscn")
 
-@export var movementPoints = 4
+@export var playermovementPoints = 4
+@export var enemymovementPoints = 3
+@export var enemyattackrange = 2
 
 var selected = false
-var currentpos
+var pcurrentpos
+var ecurrentpos
 var moving = false
 var redraw = false
 var click = false
 var cells
 var movemcellls: Array = []
+@onready var player = $player
+@onready var enemy = $enemy
+@onready var sprite = %Sprite3D
+@onready var langeliai = $Node3D
 
 func _ready():
-	$MeshInstance3D.position = Vector3(1,1.5,15)
+	player.position = Vector3(1,1.5,15)
 	var pos = local_to_map(Vector3(1,1.5,15))
-	currentpos = pos
+	pcurrentpos = pos
+	enemy.position = Vector3(1,1.5,-15)
+	pos = local_to_map(Vector3(1,1.5,-15))
+	ecurrentpos = pos
 	cells = get_used_cells()
 	
 func _process(delta):
-
 	if redraw:
 		if selected:
 			redraw = false
-			var pos = map_to_local(currentpos)
+			var pos = map_to_local(pcurrentpos)
 			var possprite = pos + Vector3(0,0.01,0)
-			$Sprite3D.position = possprite
+			sprite.position = possprite
 			#draw around
-			for i in range(1, movementPoints+1):
-				var mappos = currentpos + Vector3i(0,0,i)
+			for i in range(1, playermovementPoints+1):
+				var mappos = pcurrentpos + Vector3i(0,0,i)
 				if cells.has(mappos):
 					var pos2 = map_to_local(mappos)
 					pos2 += Vector3(0,0.01,0)
 					var lang = langelis.instantiate()
-					$".".add_child(lang)
+					langeliai.add_child(lang)
 					lang.position = pos2
 					movemcellls.append(mappos)
-			for i in range(1, movementPoints+1):
-				var mappos = currentpos + Vector3i(0,0,-i)
+			for i in range(1, playermovementPoints+1):
+				var mappos = pcurrentpos + Vector3i(0,0,-i)
 				if cells.has(mappos):
 					var pos2 = map_to_local(mappos)
 					pos2 += Vector3(0,0.01,0)
 					var lang = langelis.instantiate()
-					$".".add_child(lang)
+					langeliai.add_child(lang)
 					lang.position = pos2
 					movemcellls.append(mappos)
-			for i in range(1, movementPoints+1):
-				var mappos = currentpos + Vector3i(i,0,0)
+			for i in range(1, playermovementPoints+1):
+				var mappos = pcurrentpos + Vector3i(i,0,0)
 				if cells.has(mappos):
 					var pos2 = map_to_local(mappos)
 					pos2 += Vector3(0,0.01,0)
 					var lang = langelis.instantiate()
-					$".".add_child(lang)
+					langeliai.add_child(lang)
 					lang.position = pos2
 					movemcellls.append(mappos)
-			for i in range(1, movementPoints+1):
-				var mappos = currentpos + Vector3i(-i,0,0)
+			for i in range(1, playermovementPoints+1):
+				var mappos = pcurrentpos + Vector3i(-i,0,0)
 				if cells.has(mappos):
 					var pos2 = map_to_local(mappos)
 					pos2 += Vector3(0,0.01,0)
 					var lang = langelis.instantiate()
-					$".".add_child(lang)
+					langeliai.add_child(lang)
 					lang.position = pos2
 					movemcellls.append(mappos)
-			for y in range(1, movementPoints+1):
-				for i in range(1, movementPoints+1):
-					var mappos = currentpos + Vector3i(y,0,i)
+			for y in range(1, playermovementPoints+1):
+				for i in range(1, playermovementPoints+1):
+					var mappos = pcurrentpos + Vector3i(y,0,i)
 					if cells.has(mappos):
 						var pos2 = map_to_local(mappos)
 						pos2 += Vector3(0,0.01,0)
 						var lang = langelis.instantiate()
-						$".".add_child(lang)
+						langeliai.add_child(lang)
 						lang.position = pos2
 						movemcellls.append(mappos)
-			for y in range(1, movementPoints+1):
-				for i in range(1, movementPoints+1):
-					var mappos = currentpos + Vector3i(-y,0,-i)
+			for y in range(1, playermovementPoints+1):
+				for i in range(1, playermovementPoints+1):
+					var mappos = pcurrentpos + Vector3i(-y,0,-i)
 					if cells.has(mappos):
 						var pos2 = map_to_local(mappos)
 						pos2 += Vector3(0,0.01,0)
 						var lang = langelis.instantiate()
-						$".".add_child(lang)
+						langeliai.add_child(lang)
 						lang.position = pos2
 						movemcellls.append(mappos)
-			for y in range(1, movementPoints+1):
-				for i in range(1, movementPoints+1):
-					var mappos = currentpos + Vector3i(-y,0,i)
+			for y in range(1, playermovementPoints+1):
+				for i in range(1, playermovementPoints+1):
+					var mappos = pcurrentpos + Vector3i(-y,0,i)
 					if cells.has(mappos):
 						var pos2 = map_to_local(mappos)
 						pos2 += Vector3(0,0.01,0)
 						var lang = langelis.instantiate()
-						$".".add_child(lang)
+						langeliai.add_child(lang)
 						lang.position = pos2
 						movemcellls.append(mappos)
-			for y in range(1, movementPoints+1):
-				for i in range(1, movementPoints+1):
-					var mappos = currentpos + Vector3i(y,0,-i)
+			for y in range(1, playermovementPoints+1):
+				for i in range(1, playermovementPoints+1):
+					var mappos = pcurrentpos + Vector3i(y,0,-i)
 					if cells.has(mappos):
 						var pos2 = map_to_local(mappos)
 						pos2 += Vector3(0,0.01,0)
 						var lang = langelis.instantiate()
-						$".".add_child(lang)
+						langeliai.add_child(lang)
 						lang.position = pos2
 						movemcellls.append(mappos)
-		
+			selected = true
 		if !selected:
 			redraw = false
-			$Sprite3D.position = Vector3(0,0,0)
-			for i in range(3, get_child_count()+1):
-				if $".".get_child(i-1) != null:
-					$".".get_child(i-1).queue_free()
+			sprite.position = Vector3(0,0,0)
+			for i in range(1, langeliai.get_child_count()):
+				if langeliai.get_child(i) != null:
+					langeliai.get_child(i).queue_free()
+			selected = false
 		
 	if !click && !moving && selected:
 		if Input.is_action_pressed("Click"):
-			
 			if shoot_ray() != null:
 				var langelis = shoot_ray()
 				if movemcellls.has(langelis):
@@ -122,10 +131,13 @@ func _process(delta):
 					selected = false
 					redraw = true
 					var tween = create_tween()
-					tween.tween_property($MeshInstance3D, "position", Vector3(pos.x, 1.5, pos.z), 1)
+					tween.tween_property(player, "position", Vector3(pos.x, 1.5, pos.z), 1)
 					await tween.finished
-					currentpos = langelis
+					pcurrentpos = langelis
 					moving = false
+					enemyMove()
+					
+	
 		
 func shoot_ray():
 	var camera = get_viewport().get_camera_3d()
@@ -146,11 +158,6 @@ func map_selection(selection: Dictionary):
 		var gridmap: GridMap = selection["collider"]
 		var locsel = to_local(selection.position)
 		var pos = gridmap.local_to_map(locsel)
-		#var selection_normal: Vector3 = selection.normal
-		#if selection_normal == Vector3.RIGHT or selection_normal == Vector3.BACK:
-			#pos += -Vector3i(selection_normal) + Vector3i.UP
-		#elif selection_normal == Vector3.FORWARD or selection_normal == Vector3.LEFT:
-			#pos += Vector3i.UP
 		return pos
 
 func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
@@ -164,3 +171,43 @@ func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 			redraw = true
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == false:
 			click = false
+			
+func enemyMove():
+	var ppos = pcurrentpos
+	var epos = ecurrentpos
+	var atstumasx = ppos.x - epos.x
+	var atstumasz = ppos.z - epos.z
+	var movx = ecurrentpos.x
+	var movz = ecurrentpos.z
+	
+	if abs(atstumasx) > enemyattackrange || abs(atstumasz) > enemyattackrange:
+		if abs(atstumasx) > enemyattackrange:	
+			if abs(atstumasx) <= enemymovementPoints+enemyattackrange:
+				if atstumasx > 0:
+					movx = epos.x + (abs(atstumasx)-enemyattackrange)
+				if atstumasx < 0:
+					movx = epos.x - (abs(atstumasx)-enemyattackrange)
+			else:
+				if atstumasx > 0:
+					movx = epos.x + enemymovementPoints
+				if atstumasx < 0:
+					movx = epos.x - enemymovementPoints
+		if  abs(atstumasz) > enemyattackrange:
+			if  abs(atstumasz) <= enemymovementPoints+enemyattackrange:	
+				if atstumasz > 0:
+					movz = epos.z + (abs(atstumasz)-enemyattackrange)
+				if atstumasz < 0:
+					movz = epos.z - (abs(atstumasz)-enemyattackrange)                                
+			else:
+				if atstumasz > 0:
+					movz = epos.z + enemymovementPoints
+				if atstumasz < 0:
+					movz = epos.z - enemymovementPoints
+	var locsel = map_to_local(Vector3(movx,0,movz))
+	var tween = create_tween()
+	tween.tween_property(enemy, "position", Vector3(locsel.x, 1.5, locsel.z), 1)
+	await tween.finished
+	ecurrentpos = Vector3(movx,0,movz) 
+
+		
+		
