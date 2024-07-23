cf0e9e8ede3a21f288d7252682a2f24088c31c02
EmiKrav <126260437+EmiKrav@users.noreply.github.com>
2024-07-23 22:27:55 +0300

    dijekstra zemelapyje

--- Code/zemelapis.gd
+++ Code/zemelapis.gd
@@ -2,11 +2,14 @@ extends Node3D
 
 @onready var langelis = preload("res://Bookbearers/Scenes/ejimolangelis.tscn")
 
+
 @onready var langeliai = %lang
 @onready var player = %player
-@export var playermovementPoints = 4
-@export var playerattackrange = 3
+@export var playermovementPoints = 2
 @export var playerhealth = 10
+
+var V = 22;
+var par =[]
 
 var selected = false
 var pcurrentpos : Vector3i
@@ -24,12 +27,11 @@ var skillusage = true
 var skilling = true
 
 func _ready():
-	player.position = Vector3(0.2,1.5,5)
-	var pos = $GridMap.local_to_map(Vector3(0.2,1,5))
+	player.position = Vector3(0,1,4.6)
+	var pos = $GridMap.local_to_map(Vector3(0,1,4.5))
 	pcurrentpos = pos
 	cells = $GridMap.get_used_cells() + $GridMap2.get_used_cells() + $GridMap3.get_used_cells()
-	
-	
+	mainas()
 func _process(_delta):
 	if redraw:
 		if selected:
@@ -47,6 +49,7 @@ func _process(_delta):
 		if Input.is_action_pressed("Click"):
 			if shoot_ray() != null:
 				var langelistomove = shoot_ray()
+				
 				if movemcellls.has(langelistomove):
 					$"StaticBody3D2/Camera3D".current = false
 					$player/Camera3D2.current = true
@@ -79,6 +82,7 @@ func map_selection(selection: Dictionary):
 		var gridmap: GridMap = selection["collider"]
 		var locsel = to_local(selection.position)
 		var pos = gridmap.local_to_map(locsel)
+		
 		return pos
 
 func _on_area_3d_input_event(_camera, event, _position, _normal, _shape_idx):
@@ -134,18 +138,21 @@ func showMovement():
 			var lang = langelis.instantiate()
 			langeliai.add_child(lang)
 			lang.position = pos2
+			lang.get_child(0).text = str(i)
 		if $GridMap2.get_used_cells().has(i):
 			var pos2 = $GridMap2.map_to_local(i)
 			pos2 += Vector3(0,0.605,0)
 			var lang = langelis.instantiate()
 			langeliai.add_child(lang)
 			lang.position = pos2
+			lang.get_child(0).text = str(i)
 		if $GridMap3.get_used_cells().has(i):
 			var pos2 = $GridMap3.map_to_local(i)
 			pos2 += Vector3(0,0.605,0)
 			var lang = langelis.instantiate()
 			langeliai.add_child(lang)
 			lang.position = pos2
+			lang.get_child(0).text = str(i)
 
 func movethere(pos, langelistomove):
 	moving = true
@@ -153,7 +160,7 @@ func movethere(pos, langelistomove):
 	redraw = true
 	canmove = false
 	var tween = create_tween()
-	tween.tween_property(player, "position", Vector3(pos.x, 1.5, pos.z), 1)
+	tween.tween_property(player, "position", Vector3(pos.x, 1, pos.z), 1)
 	await tween.finished
 	$player/Camera3D2.current = false
 	$"StaticBody3D2/Camera3D".current = true
@@ -173,3 +180,65 @@ func _on_texture_rect_pressed():
 		skillusage = true
 		$"CanvasLayer/Panel/TextureRect"["disabled"]= false
 		$"CanvasLayer/Panel/TextureRect"["self_modulate"] = "ffffff"
+		
+func  minDistance(dist, sptSet):
+	var min = 9223372036854775807
+	var min_index = -1
+	for v in range(0, V):
+		if (sptSet[v] == false && dist[v] <= min):
+			min = dist[v];
+			min_index = v;
+			
+	return min_index;
+	
+func printsol(dist,src,near):
+	print("Vertex  Distance from Source: ", src);
+	for i in range(0, V):
+		print(src,"->",i,"\t\t ",dist[i])
+		printPath(i,near)
+		print(par)
+		par.clear()
+func printPath(currentVertex,parents):
+	if (currentVertex == null):
+		return; 
+	printPath(parents[currentVertex], parents); 
+	par.append(currentVertex)
+	#print(currentVertex," "); 
+	
+func dijekstra(graph, src):
+	var dist = []
+	var sptSet= []
+	var near =[]
+	near.resize(V)
+	for i in range(0, V):
+		dist.append(9223372036854775807)
+		sptSet.append(false)
+	
+	dist[src] = 0;
+	for count in range(0, V-1):
+		var u = minDistance(dist, sptSet);
+		
+		sptSet[u] = true;
+		
+		for v in range(0, V):
+			if (!sptSet[v] and graph[u][v] != 0 and dist[u] != 9223372036854775807 
+				and dist[u] + graph[u][v] < dist[v]):
+				dist[v] = dist[u] + graph[u][v];
+				near[v] = u
+	printsol(dist,src,near);
+	
+func mainas():    
+	var number = []
+	var file = FileAccess.open("res://Bookbearers/Code/grafas.txt", FileAccess.READ)
+	var content = file.get_as_text()
+	var salia = str_to_var(content)
+	#print(salia)
+	for b in range(0, V):
+		number.append([])
+		for i in range(0, V):
+			if salia[b][1].has(i):
+				number[b].append(salia[b][2][salia[b][1].find(i)])
+			else:
+				number[b].append(0)
+			
+	dijekstra(number, 0);
