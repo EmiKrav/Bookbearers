3d17601f6605b2b10c2c558558f96b21bca87614
EmiKrav <126260437+EmiKrav@users.noreply.github.com>
2024-11-14 01:23:11 +0200

    glow effect

---
+++ Efektai/zaibas.gd
@@ -0,0 +1,25 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Letters.emitting = true;
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_letters_finished():
+	await get_tree().create_timer(1).timeout
+	$Ligthning.emitting = true;
+	$Letters.queue_free()
+
+
+func _on_ligthning_finished():
+	zaib()
+	
+func zaib():
+	await get_tree().create_timer(0.5).timeout
+	$Ligthning.emitting = true;
